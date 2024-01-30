0363d26c3d1e6659a8f69ca8f322d8e9ace2a3f9
Rfarra <157190812+Rfarra@users.noreply.github.com>
2024-01-29 21:56:11 -0600

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,28 @@
+# assigning a variable
+# variable named age with a value of 25
+age = 23
+# creating a variable named 'name'
+name = "john"
+
+# printing the variables
+print("the age is: ", age)
+print("the name is: ", name)
+# Literal
+x = 13
+print("Literal:", x)
+
+# Integer
+y = -25
+print("Integer:", y)
+
+# Float
+z = 17.5
+print("Float:", z)
+
+# String
+name = "John"
+print("String:", name)
+
+# Boolean
+is_active = True
+print("Boolean:", is_active)
