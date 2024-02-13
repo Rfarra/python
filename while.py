ff3d68e32e333c10abefd536d4829ae32a9fb143
Rfarra <157190812+Rfarra@users.noreply.github.com>
2024-02-12 20:48:21 -0600

    Add files via upload

---
+++ while.py
@@ -0,0 +1,11 @@
+bottles = 99  # Initialize the number of bottles
+
+while bottles > 0:
+    print(bottles, "bottles of beer on the wall,")
+    print(bottles, "bottle of beer!")
+    print("Take one down, pass it around,")
+    bottles -= 1  # Decrease the number of bottles by 1
+    print(bottles, "bottles of beer on the wall!\n")
+
+print("No more bottles of beer on the wall, no more bottles of beer.")
+print("Go to the store and buy some more, 99 bottles of beer on the wall!")
