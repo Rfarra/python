a74b06b0b23202dbf34261e8e13d065819c10808
Rfarra <157190812+Rfarra@users.noreply.github.com>
2024-02-12 20:04:33 -0600

    Add files via upload

---
+++ decisions.py
@@ -0,0 +1,9 @@
+# if statements
+
+age = "55"
+
+if age == "55":
+    print("user is old enough to drive")
+    print("user old enough to vote")
+    print("user can buy alcohol")
+    print("user if not eligible for senior discount")
