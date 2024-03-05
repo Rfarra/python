99c5acc25ab0bd7c8682e96fa50c7a6e7d47e489
Rfarra <157190812+Rfarra@users.noreply.github.com>
2024-03-04 23:32:57 -0600

    Add files via upload

---
+++ exceptions.py
@@ -0,0 +1,12 @@
+# Simple Python program to calculate the square of a number
+
+
+def square_number():
+ try:
+    number = input("Enter a number to square: ")
+    squared_number = int(number) ** 2
+    print(f"The square of {number} is {squared_number}.")
+ except: 
+     print("Error:Please enter valid integer")
+
+square_number()
