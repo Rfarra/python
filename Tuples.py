5ac774820c1809d5c8d584bac0402d553df4c237
Rfarra <157190812+Rfarra@users.noreply.github.com>
2024-03-04 19:08:33 -0600

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,8 @@
+def main():
+    my_tuple = ('Intro to Python', 'Advanced Python', 'Database Essentials', 'Web Development Basics', 'Data Structures in Python', 'Web Design Fundamentals')
+    for item in my_tuple:
+        print(item)
+    print("The tuple has", len(my_tuple), "items.")
+
+
+main()
