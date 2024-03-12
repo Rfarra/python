38eb99cb01b8326e6105c4b34a8d9d2464c406e3
Rfarra <157190812+Rfarra@users.noreply.github.com>
2024-03-11 22:28:43 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import Modules_in_visual_studio_code.math_operations.calculator as calculator
+
+result = calculator.add(5, 3)
+print("Addition result:", result)
+
+result = calculator.subtract(10, 4)
+print("Subtraction result:", result)
