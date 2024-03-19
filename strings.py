375964d49de70d52e5bdbec9c7ef77dbcffda679
Rfarra <157190812+Rfarra@users.noreply.github.com>
2024-03-18 19:27:01 -0500

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,12 @@
+user_input = input("Enter a character: ")
+
+while len(user_input) != 1:
+   
+    print("Please enter only one character.")
+   
+    user_input = input("Enter a character: ")
+
+ascii_value = ord(user_input)
+
+print(f"The ASCII value of {user_input} is {ascii_value}")
+
