e552bd258ddcd7171e893e74ffae3a2202814a92
Rfarra <157190812+Rfarra@users.noreply.github.com>
2024-04-01 21:33:33 -0500

    Add files via upload

---
+++ artist.py
@@ -0,0 +1,24 @@
+def main():
+    top_artists = [
+        "The Beatles",
+        "Madonna",
+        "Elton John",
+        "Elvis Presley",
+        "Mariah Carey",
+        "Stevie Wonder",
+        "Janet Jackson",
+        "Michael Jackson",
+        "Whitney Houston",
+        "Rihanna",
+    ]
+    def replace_artist(top_artists):
+     try:
+        index = int(input("Enter the index of the artist to replace: "))
+        new_artist_name = input("Enter the new artist name: ")
+        top_artists[index] = new_artist_name
+        print("Updated list:", top_artists)
+    except (ValueError, IndexError):
+    print("An input error occurred.")
+
+
+main()
